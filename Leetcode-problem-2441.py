4ef5be94e0b4911785bc39ec27994d7a09228876
melkien16 <melkieyilk@gmail.com>
2024-12-04 19:35:47 +0300

    Modify based on Leetcode criteria

--- Leetcode-problem-2441.py
+++ Leetcode-problem-2441.py
@@ -1,13 +1,7 @@
-def numbers(nums):
-    largest = -1
-    for i in nums:
-        if i > 0 and -i in nums and i > largest:
-            largest = i
-    return largest
-
-
-
-nums = [-10,8,6,7,-2,-3]
-
-
-print(numbers(nums))
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        largest = -1
+        for i in nums:
+            if i > 0 and -i in nums and i > largest:
+                largest = i
+        return largest
